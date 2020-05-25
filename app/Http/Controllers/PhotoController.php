6fbb793e17e44ba52a1fa34e1fd45e0c4aefe2a8
Dean Wilson <deanwilsonbc@gmail.com>
2020-05-25 13:24:38 -0700

    Upgrade Laravel to version 5.7.

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -5,6 +5,7 @@ use App\Keywords;
 use App\Photo;
 
 use Auth;
+use Illuminate\Http\Request;
 
 class PhotoController extends Controller
 {
@@ -26,6 +27,37 @@ class PhotoController extends Controller
         return view('photos.search');
     }
 
+    public function searchSubmit(Request $request)
+    {
+        logger("PhotoController::searchSubmit ENTER", ["Data" => $request]);
+
+        $keywordId = $request->keyword_id;
+
+        $viewPublic = $request->public_checkbox;
+        $viewPublic = $viewPublic ? true : false;
+
+        $viewPrivate = $request->private_checkbox;
+        $viewPrivate = $viewPrivate ? true : false;
+
+        $text = $request->text_search;
+        if($text == null) {
+            $text="";
+        }
+
+        $fromDate = $request->from_date ? $request->from_date : "";
+        $toDate = $request->to_date ? $request->to_date : "";
+
+        return view('photos.search-results',
+                    [
+                        'keywordId' => $keywordId,
+                        'text' => $text,
+                        'publicPhotos' => $viewPublic,
+                        'privatePhotos' => $viewPrivate,
+                        'fromDate' => $fromDate,
+                        'toDate'   => $toDate
+                    ]);
+    }
+    
     public function explore()
     {
         return view('photos.explore');
@@ -37,7 +69,7 @@ class PhotoController extends Controller
             $userId = Auth::id();
             $photo = Photo::getForUser($userId, $id);
 
-            if(count($photo)) {
+            if($photo) {
                 $keywords = Keywords::findKeywordsForPhoto($id);
                 logger("Keywords are ", ["Keywords" => $keywords]);
                 return view('photos.single', ['name' => $photo->name, 'description' => $photo->description, 'src' => $photo->filepath, 'id' => $photo->id, 'keywords' => $keywords]);
