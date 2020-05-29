3abeb36911aaf2e73df00e6a9fa451c335b732c8
Dean Wilson <deanwilsonbc@gmail.com>
2020-05-28 21:08:21 -0700

    Layout fixes for single photo view.

--- resources/views/photos/single.blade.php
+++ resources/views/photos/single.blade.php
@@ -16,28 +16,31 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-12">
-                    <div class="img-area">
+                <div class="col-xs-12 col-md-10">
+                    <div class="img-area" style="text-align: center">
                        <img src="{{$src}}" alt="{{$name}}" class="responsive-image">
-                        <textarea id="desc-text" class="img-desc" name="textarea" rows="3" cols="40" onfocus="showUpdateButton()">{{$description}}</textarea>
-                        <button id="desc-update-button" class="btn btn-primary btn-sm" type="button" onclick="submitDescription({{$id}})" style="display: none">Update</button>
+                    </div>
+
+                    <h3>Description</h3>
+                    <textarea id="desc-text" class="img-desc form-control" name="textarea" rows="4" cols="40" onfocus="showUpdateButton()">{{$description}}</textarea>
+                    <button id="desc-update-button" class="btn btn-primary btn-sm" type="button" onclick="submitDescription({{$id}})" style="display: none">Update</button>
+                </div>
+                <div class="col-xs-12 col-md-2">
+                    <div id="keyword-div">
                         <h3>Keywords <button class="btn btn-xs btn-primary" type="button" onclick="showAddKeyword()">+</button></h3>
                         <div id="add-keyword-form">
                             <input id="keyword-input" type="text" size="20">
                             <button id="keyword-update-btn" class="btn btn-primary btn-sm" type="button" onclick="submitKeyword({{$id}})">Add</button>
                             <button id="keyword-done-btn" class="btn btn-primary btn-sm" type="button" onclick="doneAddKeyword()">Done</button>
                         </div>
-
-                        <div id="keyword-div">
                         @foreach($keywords as $keyword)
-                        <button class="btn">{{$keyword->name}}</button>
+                            <p><button class="btn">{{$keyword->name}}</button></p>
                         @endforeach
-                        </div>
+                    </div>
 
-                        <div id="public-toggle-div">
-                            <input type="checkbox" id="public-checkbox" name="public-checkbox" onchange="submitTogglePublic({{$id}})">
-                            <label for="subscribeNews">Allow public views.</label>
-                        </div>
+                    <div id="public-toggle-div">
+                        <input type="checkbox" id="public-checkbox" name="public-checkbox" onchange="submitTogglePublic({{$id}})">
+                        <label for="subscribeNews">Allow public views.</label>
                     </div>
                 </div>
             </div>
