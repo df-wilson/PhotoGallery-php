fdc1be865f176a53d53be55c430ca28b06fbd0b3
Dean Wilson <deanwilsonbc@gmail.com>
2020-08-19 17:40:56 -0700

    Only show abbreviated description for thumbnails when the description is to large to display fully.

--- public/js/photoExplore.js
+++ public/js/photoExplore.js
@@ -60,7 +60,11 @@ Vue.component('photo-explore', {
     },
     filters: {
         truncate(value) {
-            return value.substring(0, 70) + " ...";
+            if(value.length > 70) {
+                return value.substring(0, 66) + " ...";
+            } else {
+                return value;
+            }
         }
     },
     methods: {
